dda46de6ac416ee9dc1cce4543d52e5a3b7ddc21
Mahimai Raja J <mahimairaja3@gmail.com>
2022-06-01 01:32:22 +0530

    Know more about your image

---
+++ Image_Metadata/main.py
@@ -0,0 +1,31 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+imagename = "me.jpg"   # Image path 
+image = Image.open(imagename)
+
+info_dict = {
+    "Filename": image.filename,
+    "Image Size": image.size,
+    "Image Height": image.height,
+    "Image Width": image.width,
+    "Image Format": image.format,
+    "Image Mode": image.mode,
+    "Image is Animated": getattr(image, "is_animated", False),
+    "Frames in Image": getattr(image, "n_frames", 1)
+}
+
+for label,value in info_dict.items():
+    print(f"{label:25}: {value}")     # This 25 is just mentioning the space between
+
+
+exifdata = image.getexif()    # This getexit() returns the actual metadata
+
+for tag_id in exifdata:
+    # get the tag name, instead of human unreadable tag id
+    tag = TAGS.get(tag_id, tag_id)
+    data = exifdata.get(tag_id)
+    # decode bytes 
+    if isinstance(data, bytes):
+        data = data.decode()
+    print(f"{tag:25}: {data}")  # This 25 is just mentioning the space between
